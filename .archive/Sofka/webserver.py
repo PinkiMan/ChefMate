8857f8976c9815413d7329f8d428cb10fec5b2aa
sofie <skonvalinova@seznam.cz>
2025-05-02 21:53:59 +0200

    02.05.2025 - 21:55 Sofi - WIN_10
    Changes:
    changes made to: recipe_adder_Sofka.html
    
    added webpage idea: remake web, add new colums

--- .archive/Sofka/webserver.py
+++ .archive/Sofka/webserver.py
@@ -19,32 +19,11 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os, sys
 
-from utils.database import CookBook
-
-
-app = Flask(__name__, template_folder="../data/template", static_folder="../data/static")
-
+app = Flask(__name__, template_folder="./template", static_folder="./static")
 
 @app.route('/', methods=["POST", "GET"])
 def homepage():
-    cook_book = CookBook()
 
-    # TODO: recommender get receipts
-
-    best_trio = [cook_book.recipe_book.recipe_list[0], cook_book.recipe_book.recipe_list[0], cook_book.recipe_book.recipe_list[0]]
-
-    return render_template('lobby_flask.html', recipe_list=cook_book.recipe_book.recipe_list)
-
-@app.route('/recipe_adder.html',methods=["POST", "GET"])
-def recipe_adder():
-    return render_template('recipe_adder.html')
-
-
-@app.route('/upload')
-def upload_file():
-    return render_template('Uploader.html')
-
-
-
+    return render_template('recipe_adder_Sofka.html')
 
 app.run(host='0.0.0.0', port=8093)
